ae4c1558746947f7964acf0f4b554f6303486300
Luigi0089 <luigifravasile@gmail.com>
2026-04-23 10:16:37 +0200

    reindirizzamento al nuovo dominio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  appDomain: 'https://web-dot-personalsite-491208.oa.r.appspot.com',
   baseApiUrl:'https://vasilepersonalsitebe.onrender.com/luigi/vasile/personal/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  appDomain: 'http://localhost:4200',
   baseApiUrl: 'http://localhost:8080/luigi/vasile/personal/api'
   //baseApiUrl:'https://vasilepersonalsitebe.onrender.com/luigi/vasile/personal/api'
 
